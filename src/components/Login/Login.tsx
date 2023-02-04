8f28172f9e36714ad2bbb3ad486239fbf36bb566
Zeyad Zewail <ZeyadZewail@gmail.com>
2023-02-04 22:17:08 +0200

    finished login screen

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,21 +1,28 @@
+import { Box, TextField, Button, Paper, Alert } from "@mui/material";
 import { Auth } from "aws-amplify";
 import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import logo from "../NavBar/logo.png";
 
 interface loginInterface {
 	setLogin: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const Login: FC<loginInterface> = ({ setLogin }) => {
-	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [error, setError] = useState<Error>();
 	const navigate = useNavigate();
 
 	const handleSubmit = async (event: React.FormEvent) => {
 		event.preventDefault();
+		if (email == "" || password === "") {
+			setError(Error("Incorrect username or password."));
+			return;
+		}
+
 		try {
-			await Auth.signIn(username, password);
+			await Auth.signIn(email, password);
 			console.log("Login successful");
 			setLogin(true);
 			navigate("/");
@@ -25,12 +32,57 @@ const Login: FC<loginInterface> = ({ setLogin }) => {
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-			<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-			<button type="submit">Login</button>
-			{error && <p style={{ color: "red" }}>{error.message}</p>}
-		</form>
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+				width: "100%",
+				backgroundColor: "#D5E0FF",
+			}}>
+			<Paper
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					width: "25%",
+					padding: 4,
+					alignItems: "center",
+				}}
+				component="form"
+				onSubmit={handleSubmit}>
+				{/* logo */}
+				<Box sx={{ m: 2, marginBottom: 4 }}>
+					<img style={{ width: "239px", height: "62px" }} src={logo}></img>
+				</Box>
+				{error ? <Alert severity="error">{error.message}</Alert> : null}
+				<TextField
+					margin="normal"
+					fullWidth
+					id="email"
+					label="Email Address"
+					name="email"
+					autoComplete="email"
+					autoFocus
+					onChange={(e) => setEmail(e.target.value)}
+					placeholder="Email Address"
+				/>
+				<TextField
+					margin="normal"
+					fullWidth
+					name="password"
+					label="Password"
+					type="password"
+					id="password"
+					autoComplete="current-password"
+					onChange={(e) => setPassword(e.target.value)}
+					placeholder="Password"
+				/>
+				<Button type="submit" variant="contained" sx={{ mt: 3, mb: 2, width: "fit-content", alignSelf: "center" }}>
+					Sign In
+				</Button>
+			</Paper>
+		</Box>
 	);
 };
 
